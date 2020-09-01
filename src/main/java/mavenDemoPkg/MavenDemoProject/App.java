1688fd4010d2e6de07714004163a778b2b565b34
Rahul34754 <rahulpassi754@gmail.com>
2020-09-01 11:50:11 +0530

    Maven Project added

---
+++ src/main/java/mavenDemoPkg/MavenDemoProject/App.java
@@ -0,0 +1,13 @@
+package mavenDemoPkg.MavenDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
